e21648c9ccb58fc90c07f5e3378252789f36f766
오유정 <103317018+ujeongoh@users.noreply.github.com>
2023-04-09 13:27:18 +0900

    [Silver II] Title: 랜선 자르기, Time: 452 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,23 @@
+import sys
+
+k, n = map(int, sys.stdin.readline().split())
+lines = [int(input()) for _ in range(k)]
+
+answer = 0
+left, right = 1, max(lines)
+
+while left <= right:
+    mid = (left + right) // 2
+    if mid == 0: break
+        
+    lan_cnt = 0
+    for line in lines:
+        lan_cnt += line // mid
+
+    if lan_cnt >= n:
+        left = mid + 1
+        answer = mid
+    else:
+        right = mid - 1
+
+print(answer)
