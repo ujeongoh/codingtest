65c3e863686aa216b07f89481c8703584d0f27e8
ohyujeong <103317018+ujeongoh@users.noreply.github.com>
2023-06-04 04:29:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 108 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    n, k = map(int, input().split())
+    max = 100001
+    visited = [0] * max
+
+    def bfs():
+        q = deque([n]) # 출발
+        while q:
+            loc = q.popleft()
+            if loc == k:
+                print(visited[k])
+                break
+            for i in (loc - 1, loc + 1, loc * 2):
+                if 0 <= i < max and not visited[i]:
+                    visited[i] = visited[loc] + 1
+                    q.append(i)
+        return
+    bfs()
